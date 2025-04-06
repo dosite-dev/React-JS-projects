a7e1956a8acf665dcafa54919fb845143010ef18
dosite-dev <dositecyiza@gmail.com>
2025-04-06 03:33:01 +0200

    Diplaying object in list and sorting keys

--- styling-react-component-with-pure-css/styling-react-component/src/List.jsx
+++ styling-react-component-with-pure-css/styling-react-component/src/List.jsx
@@ -1,6 +1,19 @@
 function List(){
-    const fruits =['orange','pineApple','mango']
-    const listItems = fruits.map(fruit => <li>{fruit}</li>)
+    const fruits =[{id:1, name:'orange',calories:20},
+                    {id:2,name:'pineApple',calories:90},
+                    { id:3, name:'mango',calories:60}
+                ]
+    fruits.sort((a,b)=>a.name.localeCompare(b.name))//Alphabetical
+    fruits.sort((a,b)=>b.name.localeCompare(a.name))//Reverse Alphabetical
+    fruits.sort((a,b)=>a.calories-b.calories)//numbers
+
+
+    const listItems = fruits.map(fruit => <li key={fruit.name}>
+                                            {fruit.name}:&nbsp;
+                                            <b>{fruit.calories}</b>
+                                        </li>)
+
+
     return( <ol>{listItems}</ol>)
 
 }
